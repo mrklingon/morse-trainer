00d1bf736b17e80d74719e5e47d045e731d209a9
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-02-08 19:53:26 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString(code)
+    basic.showString("" + (alphabet[morse.indexOf(code)]))
+    code = ""
+})
 let timed = 0
-let morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"]
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let alphabet: string[] = []
+let morse: string[] = []
+let code = ""
+code = ""
+morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"]
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 basic.forever(function () {
     timed = 0
     while (input.buttonIsPressed(Button.B)) {
         timed += 1
-        basic.pause(50)
+        basic.pause(40)
     }
     if (timed < 10 && timed > 0) {
         basic.showString(".")
+        code = "" + code + "."
     }
     if (timed > 10 && timed > 0) {
         basic.showString("-")
+        code = "" + code + "-"
     }
-    basic.pause(25)
+    basic.pause(10)
     basic.showLeds(`
         . . . . .
         . . . . .
