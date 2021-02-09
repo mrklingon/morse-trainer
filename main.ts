e888130819d55f1928f17ea5601e1737bd6320a1
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-02-08 20:13:51 -0600

    builds word and displays them

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString(code)
     basic.showString("" + (alphabet[morse.indexOf(code)]))
+    Cword = "" + Cword + code
+    Word = "" + Word + alphabet[morse.indexOf(code)]
     code = ""
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString(Cword)
+    basic.pause(100)
+    basic.showString(Word)
+    Cword = ""
+    Word = ""
 })
 let timed = 0
 let alphabet: string[] = []
 let morse: string[] = []
 let code = ""
+let Cword = ""
+let Word = ""
+Word = ""
+Cword = ""
 code = ""
 morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"]
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
